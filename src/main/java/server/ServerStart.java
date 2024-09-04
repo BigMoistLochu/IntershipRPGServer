0dc0b33cfa21033863015636de052ccdae612581
BigMoistLochu <krok.konrad1@gmail.com>
2024-09-05 01:01:24 +0200

    added Dockerfile to build server, refactored code

--- src/main/java/server/ServerStart.java
+++ src/main/java/server/ServerStart.java
@@ -14,20 +14,16 @@ public class ServerStart {
         try {
             //localhost:8080/
             //"Tworzymy Socket na naszym komputerze ktory bedzie nasluchiwal na porcie 8080"
+
             ServerSocket socket = new ServerSocket(8080);
-            Socket client = socket.accept();
 
-            GameClient gameClient = new GameClient(client);
-            new Thread(gameClient);
+            while (true) {
+                Socket client = socket.accept();
 
+                GameClient gameClient = new GameClient(client);
+                new Thread(gameClient).start();
+            }
 
-            //czekamy na wiadomosc od klienta
-            InputStream inputStream = client.getInputStream();
-            //wysylamy wiadomosc do klienta
-            OutputStream outputStream = client.getOutputStream();
-
-
-            System.out.println(getMessage(inputStream));
 
         }catch (IOException e){
             System.out.println("Cos nie tak ze streamem");
@@ -35,14 +31,4 @@ public class ServerStart {
     }
 
 
-    public static StringBuilder getMessage(InputStream inputStream) throws IOException {
-        StringBuilder stringBuilder = new StringBuilder();
-        int _byte;
-        while ((_byte=inputStream.read())>=0){
-            stringBuilder.append((char) _byte);
-        }
-
-
-        return stringBuilder;
-    }
 }
--- src/main/java/server/client/GameClient.java
+++ src/main/java/server/client/GameClient.java
@@ -1,23 +1,45 @@
 package server.client;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.Socket;
 
 public class GameClient implements Runnable{
 
-    private final Socket socket;
+    private final Socket client;
 
-    public GameClient(Socket socket){
-        this.socket = socket;
+    public GameClient(Socket client){
+        this.client = client;
     }
 
     @Override
     public void run() {
-        System.out.println("start nowego watku");
-        //input
-        //output
+        System.out.println("start nowego watku: " + Thread.currentThread());
 
+        try {
+            InputStream inputStream = client.getInputStream();
+            //wysylamy wiadomosc do klienta
+            OutputStream outputStream = client.getOutputStream();
+
+            System.out.println(getMessage(inputStream));
+
+        }catch (IOException e){
+            System.out.println("blad komunikacja");
+        }
 
     }
 
-    //
+
+    public StringBuilder getMessage(InputStream inputStream) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        int _byte;
+        while ((_byte=inputStream.read())>=0){
+            stringBuilder.append((char) _byte);
+        }
+
+
+        return stringBuilder;
+    }
+
 }
