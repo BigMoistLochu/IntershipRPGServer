33ad5c6efbb1d6f13325ca9fbd056e2d64224f17
BigMoistLochu <krok.konrad1@gmail.com>
2024-09-17 18:36:02 +0200

    removed unused imports

--- src/main/java/server/ServerStart.java
+++ src/main/java/server/ServerStart.java
@@ -1,6 +1,10 @@
 package server;
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ServerStart {
     public static void main(String[] args) throws InterruptedException {
         Servlet servlet = new Servlet(8080);
